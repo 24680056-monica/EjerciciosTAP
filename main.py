44d04d8555d54440cdcbd422f6f79f9713d7c816
MonicaAnzurez <24680056@cuautla.tecnm.mx>
2026-02-08 13:48:48 -0600

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ def main(page: ft.Page):
     page.window_height = 400
     page.padding = 20
 
+
+    display_text = ft.Text("0",color=ft.Colors.WHITE,size=30)
     display = ft.Container (
-        content=ft.Text("0",color=ft.Colors.WHITE,size=30),
+        content=display_text,
         bgcolor=ft.Colors.BLACK12,
         border_radius=8,
         alignment=ft.alignment.Alignment(1, 0),
@@ -17,7 +19,20 @@ def main(page: ft.Page):
         width=210,
         height=70,
        )
+    
+    def button_clicked(e: ft.ControlEvent):
+      value = e.control.data
+      print(f"Button clicked with data = {value}")
+      if value == "AC":
+        display_text.value = "0"
+      if value == "1":
+         display_text.value+="1"     
+      if value == "2":
+         display_text.value+="2"
+      if value == "3":
+         display_text.value+="3"
 
+      page.update()
 
     grid = ft.GridView(
         runs_count=2,
@@ -30,37 +45,35 @@ def main(page: ft.Page):
 
     grid.controls.append(
         ft.Container(
-            ft.Button("1"), 
+            ft.Button("1",data="1",on_click=button_clicked),
             height=50, 
             bgcolor=ft.Colors.PURPLE_100, 
-            border_radius=8)
+            border_radius=8),
             )
     grid.controls.append(
         ft.Container(
-            ft.Button("2"),
+            ft.Button("2",data="2",on_click=button_clicked),
             height=50,
             bgcolor=ft.Colors.LIGHT_BLUE_100, 
             border_radius=8
             ))
     grid.controls.append(
         ft.Container(
-            ft.Button("3"),
+            ft.Button("3",data="3",on_click=button_clicked),
             height=50, 
             bgcolor=ft.Colors.AMBER_100,
             border_radius=8))
     grid.controls.append(
         ft.Container(
-            ft.Button("AC"),
+            ft.Button("AC",data="AC",on_click=button_clicked),
             height=50,
             bgcolor=ft.Colors.GREEN_100,
             border_radius=8))
     
-
     layout_principal = ft.Column(
         controls=[
             display,
-            grid
-            
+            grid   
         ],
         tight=True
     )
